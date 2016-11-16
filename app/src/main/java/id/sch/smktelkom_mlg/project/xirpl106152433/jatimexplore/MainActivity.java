f1ab6dd5485a7a78789d0e833dda664c9a3513fd
Vira Meliana Agustin <viramelianaagustin@gmail.com>
2016-11-16 19:56:27 +0700

    push vira 2

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl106152433/jatimexplore/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl106152433/jatimexplore/MainActivity.java
@@ -1,12 +1,16 @@
 package id.sch.smktelkom_mlg.project.xirpl106152433.jatimexplore;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,19 +23,33 @@ import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+
+import id.sch.smktelkom_mlg.project.xirpl106152433.jatimexplore.adapter.KotaHomeAdapter;
+import id.sch.smktelkom_mlg.project.xirpl106152433.jatimexplore.model.ListHome;
 
 public class MainActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
     String kota;
+    ArrayList<ListHome> mList = new ArrayList<>();
+    KotaHomeAdapter mAdapter;
     private SliderLayout mDemoSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         mDemoSlider = (SliderLayout) findViewById(R.id.slider);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new KotaHomeAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+        fillData();
+
 
         HashMap<String, Integer> file_maps = new HashMap<String, Integer>();
         file_maps.put("Surabaya", R.drawable.sby);
@@ -78,6 +96,24 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
             }
         });
     }
+
+    private void fillData() {
+        Resources resources = getResources();
+        String[] arKota = resources.getStringArray(R.array.places);
+        String[] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        TypedArray a = resources.obtainTypedArray(R.array.city_picture);
+        Drawable[] arFoto = new Drawable[a.length()];
+        for (int i = 0; i < arFoto.length; i++) {
+            arFoto[i] = a.getDrawable(i);
+        }
+        a.recycle();
+        for (int i = 0; i < arKota.length; i++) {
+            mList.add(new ListHome(arKota[i], arDeskripsi[i], arFoto[i]));
+        }
+        mAdapter.notifyDataSetChanged();
+
+    }
+
     @Override
     protected void onStop() {
         // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
